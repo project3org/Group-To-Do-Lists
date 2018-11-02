0c6908ef3534bc7797f3cf0f7aed78701c169e2a
corey-mitchell <corey69_96@yahoo.com>
2018-11-02 08:47:09 -0500

    have it successfully sending emails. need to use a different transporter. saving to fix up the front end a little bit.

--- server/mailer/config.js
+++ server/mailer/config.js
@@ -1,4 +1,7 @@
+// This file safely stores my Mailgun info
+
+// Export Mailgun information
 module.exports = {
-    MAILGUN_USER: ''
-    MAILGUN_PASS: ''
+    MAILGUN_USER:'postmaster@sandboxefe7c4f5036e49f7be6487216659885c.mailgun.org',
+    MAILGUN_PASS:'29902963725ef0376c3b58b4a440f7a9-c9270c97-af11d12e'
 };
