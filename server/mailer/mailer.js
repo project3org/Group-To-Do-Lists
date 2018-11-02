39bd10d1048529779f8b8bc24dbe7d8b5893ed92
corey-mitchell <corey69_96@yahoo.com>
2018-11-02 15:59:54 -0500

    all but done with the email verification... I need to submit an error in the repo to help fix my transport. Then fix the redirect and that should be it. After over 12 hours, I am setting this down...

--- server/mailer/mailer.js
+++ server/mailer/mailer.js
@@ -2,28 +2,32 @@
 
 // Require dependencies
 const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 const config = require('./config');
 
 // Create Transport
 const transport = nodemailer.createTransport({
-    service: 'Mailgun',
+    service: "gmail.com",
     auth: {
-        user: config.MAILGUN_USER,
-        pass: config.MAILGUN_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
+        xoauth2: xoauth2.createXOAuth2Generator({
+            type: "OAuth2",
+            user: "corey.waitforit.mitchell@gmail.com",
+            clientId: "1069498841349-bjbtrdgshq46ghj38d822970mvvicl70.apps.googleusercontent.com",
+            clientSecret: "CR20Z1V36vuHhrpd1drYoSSA",
+            // refreshToken: "REFRESH_TOKEN_HERE"
+        })                              
     }
 });
 
 // Exports sendMail function
 module.exports = {
+    // Sends Email
     sendEmail(from, to, subject, html) {
         return new Promise((resolve, reject) => {
-            transport.sendMail({ from, subject, to, html }, (err, info) => {
+            transport.sendMail({ from, to, subject, html }, (err, info) => {
                 if (err) reject(err);
 
-                resolve(info);
+                return resolve(info);
             });
         });
     }
