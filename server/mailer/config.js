d5070625cdbe86f1e17c3947346b0a16b5f02aaa
byt3_h34d <mr.tybopp@gmail.com>
2018-11-05 15:14:44 -0600

    Commiting changes before I can pull from master.

--- server/mailer/config.js
+++ server/mailer/config.js
@@ -2,11 +2,6 @@
 
 // Export Mailgun information
 module.exports = {
-<<<<<<< HEAD
     USER:'nigerianprincelookingformoney@gmail.com',
     PASS:'macaroniman69$'
-=======
-    USER:'corey.waitforit.mitchell@gmail.com',
-    PASS:'athena18$'
->>>>>>> dece68a9df512668c4094b76ec0e68aebcb67d64
 };
