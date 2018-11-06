78ad1c7411a4879360909708976a1d4b9f82ac70
byt3_h34d <mr.tybopp@gmail.com>
2018-11-05 22:09:26 -0600

    Updating language utils>API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all tasks
   getTasks: function() {
     return axios.get("/api/tasks");
   },
-  // Gets the book with the given id
+  // Gets the task with the given id
   getTask: function(id) {
     return axios.get("/api/tasks/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the task with the given id
   deleteTask: function(id) {
     return axios.delete("/api/tasks/" + id);
   },
-  // Saves a book to the database
+  // Saves a task to the database
   saveTask: function(taskData) {
     return axios.post("/api/tasks", taskData);
   }
