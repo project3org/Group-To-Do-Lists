093b18e5623ac690d3109d4a46e68604c50dc87b
corey-mitchell <corey69_96@yahoo.com>
2018-11-16 12:46:43 -0600

    still messing with heroku. none of the online recommendations seem to be doing any good.

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname + './client/build/index.html'));
+  });
 };
 
 // Add routes, both API and view
@@ -20,7 +24,7 @@ app.use(routes);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname + './client/build/index.html'));
+  res.sendFile(path.join(__dirname + './client/public/index.html'));
 });
 
 // If deployed, use the deployed database. Otherwise use the local gratify database
