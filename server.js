e66d840baf7ace925f007940e3f9cdd6b4a3ee32
corey-mitchell <corey69_96@yahoo.com>
2018-11-16 07:56:51 -0600

    Trying to fix react router dom with heroku again. The other issues have been sorted out.

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 };
 
 // Add routes, both API and view
