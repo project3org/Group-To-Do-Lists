78b5a231e4deab597f7355b126e85356c5c7c858
corey-mitchell <corey69_96@yahoo.com>
2018-11-16 09:26:04 -0600

    saving to get TA help... This one is beyond my skills...

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./server/routes");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
 };
 
 // Add routes, both API and view
