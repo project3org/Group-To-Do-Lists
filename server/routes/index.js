68e938b9077ee25c897e2c479c419f4484b42745
corey-mitchell <corey69_96@yahoo.com>
2018-11-17 10:03:10 -0600

    added router route for email confirmation.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,11 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+// Route for email confirmation
+router.route('/account/confirmation/:token').get(function(req, res) {
+  res.sendFile(path.resolve('../../client/public/index.html'));
+});
+
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../../client/public/index.html"));
